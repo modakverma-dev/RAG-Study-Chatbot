aee6656c25f645c33a6824dbb0c1cb4c76a30c68
modakverma-dev <modakverma.dev@gmail.com>
2025-09-02 15:52:58 +0530

    loading and creating embeddings of the document

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="study_chatbot",
+    version="0.1.0",
+    author="Modak Verma",
+    author_email="modakverma.dev@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
